0cc221796e47ab8db056ff7d6ccbb070c4edd439
Celine <rat.celine@outlook.com>
2023-01-24 21:00:53 +0100

    correction typo

--- sql/007-data-choices.sql
+++ sql/007-data-choices.sql
@@ -1,22 +1,22 @@
 --Choices
 --C1
-insert into questions (name, question_type)
+insert into choices (name, question_id)
 	values ('Très bien', 1);
 --C2
-insert into questions (name, question_type)
+insert into choices (name, question_id)
 	values ('Bien', 1);
 --C3
-insert into questions (name, question_type)
+insert into choices (name, question_id)
 	values ('Moyennement', 1);
 --C4
-insert into questions (name, question_type)
+insert into choices (name, question_id)
 	values ('Mal', 1);
 
-insert into questions (name, question_type)
+insert into choices (name, question_id)
 	values ('Vert', 2);
 
-insert into questions (name, question_type)
+insert into choices (name, question_id)
 	values ('Orange', 2);
 
-insert into questions (name, question_type)
+insert into choices (name, question_id)
 	values ('Bleu', 2);
