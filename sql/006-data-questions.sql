8c0e2fedfdec8730a5109d32c2fb2d2ad0ddcf29
Vincent Crjlt-rd <vincentcr81@hotmail.fr>
2023-02-02 09:46:30 +0100

    favorite attribute on questions

--- sql/006-data-questions.sql
+++ sql/006-data-questions.sql
@@ -1,18 +1,18 @@
 -- Questions
 --Q1
-insert into questions (title, question_type)
-	values ('DEFAULT_QUESTION_BIEN_MAL', 'QCM');
+insert into questions (title, question_type,favorite)
+	values ('DEFAULT_QUESTION_BIEN_MAL', 'QCM',true);
 --Q2
-insert into questions (title, question_type)
-	values ('DEFAULT_QUESTION_SOUVENT', 'QCM');
+insert into questions (title, question_type,favorite)
+	values ('DEFAULT_QUESTION_SOUVENT', 'QCM',true);
 
 --Q3	
-insert into questions (title, question_type)
-	values ('DEFAULT_QUESTION_YES_NO', 'YES_NO');
+insert into questions (title, question_type,favorite)
+	values ('DEFAULT_QUESTION_YES_NO', 'YES_NO',true);
 --Q4
-insert into questions (title, question_type)
-	values ('Sur quel projet travailles-tu ?', 'FREE_RESPONSE');--Q2
+insert into questions (title, question_type,favorite)
+	values ('Sur quel projet travailles-tu ?', 'FREE_RESPONSE',false);--Q2
 
 
-insert into questions (title, question_type)
-	values ('Quelle est ta couleur préférée ?', 'QCM');
+insert into questions (title, question_type,favorite)
+	values ('Quelle est ta couleur préférée ?', 'QCM',false);
